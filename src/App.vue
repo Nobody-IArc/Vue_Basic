e33edebf0dc23b1df6a431ccd005afe6b251cba0
Nobody-IArc <utk3970@naver.com>
2025-05-15 20:36:44 +0900

    컴포넌트 결합

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Head from "@/components/Head.vue";
+import Body from "@/components/Body.vue";
 </script>
 
 <template>
   <Head />
+  <Body />
 </template>
---
+++ src/components/Body.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+  <div class="body">
+    <span>Body Part</span>
+  </div>
+</template>
+
+<style scoped>
+.body {
+  border: 1px solid red;
+  border-radius: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+}
+</style>

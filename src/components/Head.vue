b7c670f4ca6faac6aa5944ee5f47dc666a39c3f9
Nobody-IArc <utk3970@naver.com>
2025-05-15 20:32:24 +0900

    Vue 컴포넌트 - 0

---
+++ src/components/Head.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+  <div class="head">
+    <span>Head Part</span>
+  </div>
+</template>
+
+<style scoped>
+.head {
+  border: 1px solid green;
+  border-radius: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 200px;
+}
+</style>

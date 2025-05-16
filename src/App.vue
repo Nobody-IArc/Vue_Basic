39fc2cea944756d68c35c5c75d7d0102bd8fa97e
Nobody-IArc <utk3970@naver.com>
2025-05-16 19:28:44 +0900

    여러 변수 유형 테스트

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <script setup>
-import Head from "@/components/Head.vue";
-import Body from "@/components/Body.vue";
+// const message = " Vue 3";
+// const message = { name: 'World', age: 30 };
+const page = "history";
+let loading = true;
 </script>
 
 <template>
-  <Head />
-  <Body />
+<!--  <div v-bind:id="page">-->
+<!--    HISTORY MAKIN-->
+<!--&lt;!&ndash;    <span>Hello </span>&ndash;&gt;-->
+<!--&lt;!&ndash;    <span v-text="JSON.stringify(message)"></span>&ndash;&gt;-->
+<!--  </div>-->
+  <input type="text" placeholder="ID" :readonly="loading" />
+  <input type="password" placeholder="Password" :readonly="loading" />
+  <button :disabled="loading">로그인</button>
 </template>

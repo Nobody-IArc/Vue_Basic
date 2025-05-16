2bf0a789393e508307f895900372aa31379a2d82
Nobody-IArc <utk3970@naver.com>
2025-05-16 23:56:08 +0900

    조건문 처리

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,11 @@
 <script setup>
-// const parent = "Fruits";
-// const child = "iten";
-// const fruits = "Apple Banana Coconut Durian";
-// const fruitsArr = ["Apple", "Banana", "Coconut", "Durian"];
-const visible = { fruits: true };
-const hidden = { fruits: false };
-// const rect = "background:blue;width:200px;height:200px;"
-const rect = {
-  background: "blue",
-  width: "200px",
-  height: "200px",
-}
+let loading = true;
 </script>
 
 <template>
-<!--  <ul :class="parent">-->
-<!--    <li :class="child">Apple</li>-->
-<!--    <li :class="child">Banana</li>-->
-<!--    <li :class="child">Coconut</li>-->
-<!--    <li :class="child">Durian</li>-->
-<!--  </ul>-->
-<!--  <div :class="fruits">사과 / 바나나</div>-->
-<!--  <div :class="fruitsArr">바나나 / 코코넛</div>-->
-<!--  <div :title="fruitsArr">코코넛 / 두리안</div>-->
-  <div :style="rect">
-  <div :class="visible">사과</div>
-  <div :class="hidden">과자</div>
-  <div :class="{ fruits: true }">바나나</div>
-  </div>
+  <template v-if="loading">On Loading...</template>
+  <template v-else>Loading Finished!</template>
+  <div v-show="loading">On Loading...</div>
+<!--  소스 코드에서는 출력되는 v-show -->
+  <div v-show="!loading">On Loading...</div>
 </template>

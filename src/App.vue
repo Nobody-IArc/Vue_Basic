89f6d924bc53e8ee157e311a40ddf6f8aa494fa8
Nobody-IArc <utk3970@naver.com>
2025-05-17 00:42:07 +0900

    반응형 - 2 reactive 메서드 사용 및 nextTick 으로 콘솔 및 웹 동기화

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,38 @@
 <script setup>
-import {ref} from "vue";
+import {nextTick, reactive} from "vue";
 
-const count = ref(0);
+const state = reactive({
+  count: 0,
+});
 
 const increaseCount = () => {
-  count.value++;
-  console.log(`count is increased to ${count}`);
+  state.count++;
+
+  nextTick(() => {
+    console.log(document.getElementById("count").innerText);
+  });
 };
 
 const decreaseCount = () => {
-  count.value--;
-  console.log(`click is decreased to ${count}`);
+  state.count--;
+
+  nextTick(() => {
+    console.log(document.getElementById("count").innerText);
+  });
 };
 </script>
 
 <template>
   <div>
-    <h1>Button click</h1>
+    <h1>숫자의 수</h1>
     <br />
     <hr />
     <ul>
-      <li id="count">숫자 세기 - {{ count }}</li>
+      <li id="count">현재의 수: {{ state.count }}</li>
     </ul>
     <hr />
     <br />
-    <button @click="increaseCount()">숫자 세기 증가</button>
-    <button @click="decreaseCount()">숫자 세기 감소</button>
+    <button @click="increaseCount()">숫자 올라가!</button>
+    <button @click="decreaseCount()">숫자 내려가!</button>
   </div>
 </template>

9e5a60b8b3fc882c0fd27b2263e0cc6a7e6639c8
Nobody-IArc <utk3970@naver.com>
2025-05-19 16:53:18 +0900

    생성 훅

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
 <script setup>
-import {reactive} from "vue";
-
-const state = reactive({
-  form: {
-    id: "",
-    pw: ""
-  }
-});
-
-const login = () => {
-  window.alert(state.form.id + "님 로그인 완료");
-}
+// 커스텀 생성 훅
+(async function onCreated() {
+  const hook = 'onCreated';
+  console.log(hook);
+})();
 </script>
 
 <template>
-  <form @submit.prevent="login">
-    <input type="text" v-model="state.form.id" placeholder="아이디" />
-    <input type="password" v-model="state.form.pw" placeholder="비밀번호" />
-
-    <button type="submit">Login</button>
-  </form>
+  <div id="message">Aloha!</div>
 </template>

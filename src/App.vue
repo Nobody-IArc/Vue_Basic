b25fcafb144458390cc1d579d33e623755057d3b
Nobody-IArc <utk3970@naver.com>
2025-05-19 13:08:17 +0900

    v-model 을 사용한 양방향 데이터 바인딩

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,6 @@ const state = reactive({
 <template>
   <div>
     <h1>{{ state.player }}</h1>
-    <input type="text" :value="state.player" />
+    <input type="text" v-model="state.player" />
   </div>
 </template>

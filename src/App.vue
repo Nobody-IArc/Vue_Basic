b5e64786b863cd7f6c4fc9e25f965643eb0d3104
Nobody-IArc <utk3970@naver.com>
2025-05-19 13:17:31 +0900

    폼 처리 양방향 데이터 바인딩, 내부 로직 미구현

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,22 @@
 import {reactive} from "vue";
 
 const state = reactive({
-  player: "Arthur"
+  form: {
+    id: "",
+    pw: ""
+  }
 });
+
+const login = () => {
+  window.alert(state.form.id + "님 로그인 완료");
+}
 </script>
 
 <template>
-  <div>
-    <h1>{{ state.player }}</h1>
-    <input type="text" v-model="state.player" />
-  </div>
+  <form @submit.prevent="login">
+    <input type="text" v-model="state.form.id" placeholder="아이디" />
+    <input type="password" v-model="state.form.pw" placeholder="비밀번호" />
+
+    <button type="submit">Login</button>
+  </form>
 </template>

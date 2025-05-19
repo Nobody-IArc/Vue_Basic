d63d9249bf33a98030971fc0a3e9248a230b8a7c
Nobody-IArc <utk3970@naver.com>
2025-05-19 13:06:18 +0900

    단방향 데이터 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,14 @@
 <script setup>
-import {computed, reactive} from "vue";
+import {reactive} from "vue";
 
 const state = reactive({
-  mvpId: 7,
-  players: [
-    { id: 7, name: 'John' },
-    { id: 9, name: 'Jane' },
-    { id: 10, name: 'Bob' },
-  ]
+  player: "Arthur"
 });
-
-const computedMvpName = computed(() => {
-  if (state.mvpId) {
-    const player = state.players.find(p => p.id === state.mvpId);
-
-    if (player) {
-      return player.name;
-    }
-  }
-
-  return 'NO MVP';
-});
-
-// .value 를 사용해야 script 블록에서 컴퓨티드 사용 가능
-console.log(computedMvpName.value);
 </script>
 
 <template>
   <div>
-    <h1>MVP</h1>
-    <div>{{ computedMvpName }}</div>
+    <h1>{{ state.player }}</h1>
+    <input type="text" :value="state.player" />
   </div>
 </template>
